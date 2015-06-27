609a15d3e0788015ced728bf7e4659d55b0c6698
Andrew Reder <avidreder@gmail.com>
2015-06-27 15:09:55 -0700

    Added Facebook, Google authentication, routes for logout and login check

---
+++ routes/auth-routes.js
@@ -0,0 +1,60 @@
+module.exports = function(app, passport){
+  function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) { return next(); }
+    res.send('Not Authorized');
+  }
+  
+  app.get('/logged-in', ensureAuthenticated, function(req, res){
+    res.send(req.user);
+  });
+
+  app.get('/logout', function(req, res){
+    req.logout();
+    res.send('Logged Out');
+  });
+
+  /*app.get('/login', function(req, res){
+    res.render('login', { user: req.user });
+  });*/
+
+  // GET /auth/google
+  //   Use passport.authenticate() as route middleware to authenticate the
+  //   request.  The first step in Google authentication will involve
+  //   redirecting the user to google.com.  After authorization, Google
+  //   will redirect the user back to this application at /auth/google/callback
+  app.get('/auth/google',
+    passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }),
+    function(req, res){
+      // The request will be redirected to Google for authentication, so this
+      // function will not be called.
+    });
+
+  // GET /auth/google/callback
+  //   Use passport.authenticate() as route middleware to authenticate the
+  //   request.  If authentication fails, the user will be redirected back to the
+  //   login page.  Otherwise, the primary route function function will be called,
+  //   which, in this example, will redirect the user to the home page.
+  app.get('/auth/google/callback', 
+    passport.authenticate('google'),
+    function(req, res) {
+      res.send(req.user);
+    });
+
+  app.get('/auth/facebook',
+    passport.authenticate('facebook', {scope: 'email'}),
+    function(req, res){
+      // The request will be redirected to facebook for authentication, so this
+      // function will not be called.
+    });
+
+  // GET /auth/facebook/callback
+  //   Use passport.authenticate() as route middleware to authenticate the
+  //   request.  If authentication fails, the user will be redirected back to the
+  //   login page.  Otherwise, the primary route function function will be called,
+  //   which, in this example, will redirect the user to the home page.
+  app.get('/auth/facebook/callback', 
+    passport.authenticate('facebook'),
+    function(req, res) {
+      res.send(req.user);
+    });
+}
