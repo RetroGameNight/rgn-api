f99d83649b60b506097827bf9cbbcafefefdee96
Andrew Reder <avidreder@gmail.com>
2015-07-09 16:14:51 -0700

    Added CORS headers to ensureAuthenticated for testing

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,6 +1,9 @@
 module.exports = function(app, passport){
   function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) { return next(); }
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.send('Not Authorized');
   }
 
