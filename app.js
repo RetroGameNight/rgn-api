d54e7b12c72c6b097e5210498166d1c494655bf9
Cameron Brandon White <cameronbwhite90@gmail.com>
2015-07-09 16:42:28 -0700

    set Access-Control-Allow-Credentials to true

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ var FacebookStrategy = require('passport-facebook').Strategy;
 var app = express();
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', 'http://localhost:8081');
+    res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
