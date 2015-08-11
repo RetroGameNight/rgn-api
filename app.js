bbfa5a8507c4d7593f7167954186b92bea618edf
Chris Loeper <chrisloeper@gmail.com>
2015-08-11 11:04:52 -0700

    This is fucking ridiculous.

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var FacebookStrategy = require('passport-facebook').Strategy;
 // Initialize Express App
 var app = express();
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', *);
+    res.header('Access-Control-Allow-Origin', 'http://retrogamenight-dev.herokuapp.com');
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
