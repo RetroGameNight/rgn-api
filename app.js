51f3c81273c548a691f5a2f3a016123654ee67b3
Chris Loeper <chrisloeper@gmail.com>
2015-08-11 10:57:25 -0700

    Brute forcing CORS

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var FacebookStrategy = require('passport-facebook').Strategy;
 // Initialize Express App
 var app = express();
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', appconfig.env.ui.url + appconfig.env.ui.port);
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
