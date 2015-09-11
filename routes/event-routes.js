b74425083d7d455ca460fa6b1859d1f17c974edc
Cameron Brandon White <cameronbwhite90@gmail.com>
2015-09-11 12:08:19 -0700

    Refactored `event-routes` to use Promises

--- routes/event-routes.js
+++ routes/event-routes.js
@@ -25,106 +25,100 @@ export default (app, rethinkdb) => {
   //app.route('/user/update').put(update)          // Update a todo
   //app.route('/user/delete').post(del)         // Delete a todo
   function listEvents(req, res, next) {
-    rethinkdb.connect(appconfig.rethinkdb, (err, conn) => {
-      rethinkdb.table('events').orderBy({index: "startTime"}).run(conn, (error, cursor) => {
-          if (error) {
-              handleError(res, error) 
-              next()
-          }
-          else {
-              // Retrieve all the todos in an array
-              cursor.toArray((error, result) => {
-                  if (error) {
-                      handleError(res, error) 
-                  }
-                  else {
-                      // Send back the data
-                      res.json(result)
-                  }
-              })
-          }
+    let connection = null
+    rethinkdb.connect(appconfig.rethinkdb)
+      .then(conn => {
+        connection = conn
+        return rethinkdb
+          .table('events')
+          .orderBy({index: "startTime"})
+          .run(connection)
       })
-    })
+      .then(cursor => cursor.toArray())
+      .then(result => res.json(result))
+      .then(() => connection.close())
+      .error(error => handleError(res, error))
   }
   function getEvent(req, res, next) {
+    let connection = null
     const eventID = req.params.id
-
-    rethinkdb.connect(appconfig.rethinkdb, (err, conn) => {
-      rethinkdb.table('events').get(eventID).run(conn, (error, result) => {
-        if(error) {
-          handleError(res, error) 
-          next()
-        }
-        else {
-          res.json(result)
-        }
+    rethinkdb.connect(appconfig.rethinkdb)
+      .then(conn => {
+        connection = conn
+        return rethinkdb
+          .table('events')
+          .get(eventID)
+          .run(conn)
       })
-    })
+      .then(result => res.json(result))
+      .then(() => connection.close())
+      .error(error => handleError(res, error))
   }
 
   function createEvent(req, res, next) {
-      const newEvent = {}
-      newEvent.startTime = req.body.startTime || rethinkdb.now()
-      newEvent.endTime = req.body.endTime || rethinkdb.now().add(14400)      // req.body was created by `bodyParser`  // Set the field `createdAt` to the current time
-      newEvent.name = req.body.name || "Unnamed Game Night"
-      newEvent.owner = req.body.owner || "Anonymous"
-      newEvent.people = req.body.people || []
-      newEvent.avatarURL = req.body.avatarURL || ""
-      newEvent.type = req.body.type || "other"         // req.body was created by `bodyParser`
-      newEvent.createdAt = rethinkdb.now()
-      newEvent.lastUpdated = rethinkdb.now()
-      rethinkdb.connect(appconfig.rethinkdb, (err, conn) => {
-        rethinkdb.table('events').insert(newEvent, {returnChanges: true}).run(conn, (error, result) => {
-            if (error) {
-              handleError(res, error)
-              next()
-            }
-            else if (result.inserted !== 1) {
-              handleError(res, new Error("Document was not inserted."))
-              next()
-            }
-            else {
-              res.json(result.changes[0].new_val)
-            } 
+      let connection = null
+      const event = {}
+      event.startTime = req.body.startTime || rethinkdb.now()
+      event.endTime = req.body.endTime || rethinkdb.now().add(14400)      // req.body was created by `bodyParser`  // Set the field `createdAt` to the current time
+      event.name = req.body.name || "Unnamed Game Night"
+      event.owner = req.body.owner || "Anonymous"
+      event.people = req.body.people || []
+      event.avatarURL = req.body.avatarURL || ""
+      event.type = req.body.type || "other"         // req.body was created by `bodyParser`
+      event.createdAt = rethinkdb.now()
+      event.lastUpdated = rethinkdb.now()
+      rethinkdb.connect(appconfig.rethinkdb)
+        .then(conn => {
+          connection = conn
+          return rethinkdb
+            .table('events')
+            .insert(event, {returnChanges: true})
+            .run(connection)
         })
-      })
+        .then(result => {
+          if (result.inserted !== 1) {
+              handleError(res, new Error("Document was not inserted."))
+          } else {
+              return res.json(result.changes[0].new_val)
+          }
+        })
+        .then(() => connection.close())
+        .error(error => handleError(res, error))
   }
 
   function updateEvent(req, res, next) {
     const eventID = req.params.id
-    const updatedEvent = {}
-    let currentEvent = {}
-    rethinkdb.connect(appconfig.rethinkdb, (err, conn) => {
-      rethinkdb.table('events').get(eventID).run(conn, (error, result) => {
-        if(error) {
-          handleError(res, error) 
-          next()
-        }
-        else {
-          currentEvent = result
-          updatedEvent.name = req.body.name || currentEvent.name
-          updatedEvent.owner = req.body.owner || currentEvent.owner
-          updatedEvent.startTime = req.body.startTime || currentEvent.startTime
-          updatedEvent.people = req.body.people || currentEvent.people
-          updatedEvent.endTime = req.body.endTime || currentEvent.endTime
-          updatedEvent.avatarURL = req.body.avatarURL || currentEvent.avatarURL
-          updatedEvent.type = req.body.type || currentEvent.type         // req.body was created by `bodyParser`
-          updatedEvent.lastUpdated = rethinkdb.now()
-          rethinkdb.connect(appconfig.rethinkdb, (err, conn) => {
-            rethinkdb.table('events').get(eventID).update(updatedEvent, {returnChanges: true}).run(conn, (error, result) => {
-              if(error) {
-                //handleError(res, error) 
-                throw error
-                next()
-              }
-              else {
-                res.json(result.changes[0].new_val)
-              }
-            })
-          })
-        }
+    let connection = null
+    rethinkdb.connect(appconfig.rethinkdb)
+      .then(conn => {
+        connection = conn
+        return rethinkdb
+          .table('events')
+          .get(eventID)
+          .run(connection)
       })
-    })
+      .then(result => {
+        const event = {}
+        const currentEvent = result
+        event.name = req.body.name || currentEvent.name
+        event.owner = req.body.owner || currentEvent.owner
+        event.startTime = req.body.startTime || currentEvent.startTime
+        event.people = req.body.people || currentEvent.people
+        event.endTime = req.body.endTime || currentEvent.endTime
+        event.avatarURL = req.body.avatarURL || currentEvent.avatarURL
+        event.type = req.body.type || currentEvent.type         // req.body was created by `bodyParser`
+        event.lastUpdated = rethinkdb.now()
+        return event
+      })
+      .then(event => rethinkdb
+        .table('events')
+        .get(eventID)
+        .update(event, {returnChanges: true})
+        .run(connection)
+      )
+      .then(result => res.json(result.changes[0].new_val))
+      .then(() => connection.close())
+      .error(error => handleError(res, error))
   }
 
   /*
@@ -133,16 +127,18 @@ export default (app, rethinkdb) => {
   function deleteEvent(req, res, next) {
     const eventID = req.params.id
 
-    rethinkdb.connect(appconfig.rethinkdb, (err, conn) => {
-      rethinkdb.table('events').get(eventID).delete().run(conn, (error, result) => {
-        if(error) {
-          handleError(res, error)
-          next()
-        }
-        else {
-          res.json({success: true})
-        }
+    let connection = null
+    rethinkdb.connect(appconfig.rethinkdb)
+      .then(conn => {
+        connection = conn
+        return rethinkdb
+          .table('events')
+          .get(eventID)
+          .delete()
+          .run(connection)
       })
-    })
+      .then(() => res.json({success: true}))
+      .then(() => connection.close())
+      .error(error => handleError(res, error))
   }
 }
